3f7a9491f07b119b4ea742e50229b95708017e37
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-04-21 16:41:17 +0100

    Load students from file

--- src/main/java/pt/esgts/brunojesus/presencas/repository/StudentRepository.java
+++ src/main/java/pt/esgts/brunojesus/presencas/repository/StudentRepository.java
@@ -2,25 +2,49 @@ package pt.esgts.brunojesus.presencas.repository;
 
 import pt.esgts.brunojesus.presencas.model.Student;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentRepository {
 
     private final List<Student> studentList;
 
     public StudentRepository() {
-        studentList = new ArrayList<>();
-        studentList.add(Student.of(593484,"Filipe Silva"));
-        studentList.add(Student.of(328345, "Filipe Sousa"));
-        studentList.add(Student.of(595904, "Rui Manuel"));
-        studentList.add(Student.of(490340, "Carlos Filipe"));
-        studentList.add(Student.of(399403, "Tiago Jorge"));
+        studentList = loadStudentsFile()
+                .stream()
+                .map(s -> s.split(","))
+                .map(s -> new Student(Long.parseLong(s[0]), s[1]))
+                .toList();
     }
 
     public List<Student> findAll() {
         return new ArrayList<>(studentList);
         //return Collections.unmodifiableList(studentList);
+    }
+
+    private List<String> loadStudentsFile() {
+        List<String> result = null;
+
+        try (InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream("students.csv")) {
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream)
+            );
+
+            result = reader
+                    .lines()
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("Failed to close file");
+        }
+
+        return result;
     }
 }
